c29845698096eff8ee2e5768028e5cad561d6af0
danielbelle <henrique.danielb@gmail.com>
2025-02-25 19:02:11 -0300

    010

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ Route::post('/', [MainController::class, 'prepareGame'])->name('prepare_game');
 // in game
 Route::get('/game', [MainController::class, 'game'])->name('game');
 Route::get('/answer/{answer}', [MainController::class, 'answer'])->name('answer');
+Route::get('/next_question', [MainController::class, 'nextQuestion'])->name('next_question');
+
+// game over
+Route::get('/show_results', [MainController::class, 'ShowResults'])->name('show_results');
